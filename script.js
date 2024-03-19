70cb2cc04a0ef9b2f2fc28f6ba25a30a84a9817e
lauracardoso1 <159258689+lauracardoso1@users.noreply.github.com>
2024-03-19 09:14:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,17 +22,27 @@ var cidade = document.getElementById("cidade");
 
 var estado = document.getElementById("estado");
 function alertar(){
+
+     //buscar o endereço pelo cep
+   const url = `https//viacep.com.br/ws/${cep.value}/json`;
+    fetch(url)
+   .then(resposta=>resposta.json())
+   .then(data => {
+    logradouro.value = data.logradouro;
+    bairro.value = data.bairro;
+    cidade.value = data.cidade;
+    estado.value = data.uf;
     //alert(nome.value + "" + "clicou no botão!!!");
-    "Nome: " + " " + nome.value;
-     "\n E-mail:" + " " + email.value;
-    "\n Telefone: " + " " + telefone.value;
-     "\n cep: " + " " + cep.value;
-     "\n logradouro: " + " " + logradouro.value;
-     "\n número: " + " " + número.value;
-     "\n complemento: " + " " + complemento.value;
-     "\n bairro: " + " " + bairro.value;
-     "\n cidade: " + " " + cidade.value;
+    saida.innerText = "Nome: " + " " + nome.value +
+     "\n E-mail:" + " " + email.value +
+     "\n Telefone: " + " " + telefone.value +
+     "\n cep: " + " " + cep.value +
+     "\n logradouro: " + " " + logradouro.value +
+     "\n número: " + " " + número.value +
+     "\n complemento: " + " " + complemento.value +
+     "\n bairro: " + " " + bairro.value +
+     "\n cidade: " + " " + cidade.value +
      "\n estado: " + " " + estado.value;
-}
-    alert("Você clicou no botão!!!");
+})
+.catch(error=>alert(error))
 }
