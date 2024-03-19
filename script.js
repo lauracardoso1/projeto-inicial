f7ffaacb12b16df358df91e4c494058a613519f1
lauracardoso1 <159258689+lauracardoso1@users.noreply.github.com>
2024-03-19 08:45:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 var botao = document.getElementById("botao");
-botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
+var telefone = document.getElementById("Telefone");
+
+var cep = document.getElementById("CEP");
+
+var logradouro = document.getElementById("logradouro");
+
+var número = document.getElementById("número");
+
+var complemento = document.getElementById("complemento");
+
+var bairro = document.getElementById("bairro");
+
+var cidade = document.getElementById("cidade");
+
+var estado = document.getElementById("estado");
 function alertar(){
+    //alert(nome.value + "" + "clicou no botão!!!");
+    "Nome: " + " " + nome.value;
+     "\n E-mail:" + " " + email.value;
+    "\n Telefone: " + " " + telefone.value;
+     "\n cep: " + " " + cep.value;
+     "\n logradouro: " + " " + logradouro.value;
+     "\n número: " + " " + número.value;
+     "\n complemento: " + " " + complemento.value;
+     "\n bairro: " + " " + bairro.value;
+     "\n cidade: " + " " + cidade.value;
+     "\n estado: " + " " + estado.value;
+}
     alert("Você clicou no botão!!!");
 }
